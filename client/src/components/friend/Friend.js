0a1323d164e48284f3063619206d73b7f05a2994
Shivam Singh <shivamsinghgbpit@gmail.com>
2023-03-05 22:29:22 +0530

    add link to profile in sidebar

--- client/src/components/friend/Friend.js
+++ client/src/components/friend/Friend.js
@@ -1,10 +1,15 @@
+import { useHistory } from "react-router-dom";
 import "./friend.css";
 const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
 export default function Friend({ user }) {
+const history = useHistory();
   return (
     <li className="sidebarFriendsListItem">
       <img
+        onClick={() => {
+          history.push("/profile/" + user.username)
+        }}
         src={
           user.profilePicture ? PF + user.profilePicture : PF + "/person/0.jpeg"
         }
