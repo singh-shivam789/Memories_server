f4bbc7c28265948415c062eb49dbf3e922b4c01e
singh-shivam789 <shivamsinghgbpit@gmail.com>
2023-03-17 01:01:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const path = require("path");
+const cors = require(cors)
 dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 let username = encodeURIComponent("shivamsingh_99");
 let password = encodeURIComponent("singh@123");
@@ -27,6 +29,7 @@ mongoose
 app.use("/files", express.static(path.join(__dirname, "public/files")));
 
 //middleware
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
